168dbe4813a15c26c7883ca6f5c6240fc5c9f5e3
Harry Shi <harryshi.cs@gmail.com>
2017-06-27 10:26:46 +0800

    update Autoencoer runner

--- AutoEncoder/code/AutoEncoder_Runner.py
+++ AutoEncoder/code/AutoEncoder_Runner.py
@@ -15,33 +15,34 @@ from tensorflow.examples.tutorials.mnist import input_data
 mnist = input_data.read_data_sets("MNIST_data", one_hot=True)
 batch_xs, batch_ys = mnist.train.next_batch(128)
 
-autoencoder = Autoencoder_2Layer(num_input = 784,
-                          num_hidden_1 = 256,
-                          num_hidden_2 = 128,
-                          act_func = tf.nn.sigmoid,
-                          optimizer = tf.train.RMSPropOptimizer(learning_rate = 0.01))
-
-training_epochs = 20
-batch_size = 256
-display_step = 1
-examples_to_show = 10
-total_batch = int(mnist.train.num_examples/batch_size)
-
-for epoch in range(training_epochs):
-    for i in range(total_batch):
-        batch_xs, batch_ys = mnist.train.next_batch(batch_size)
-        loss = autoencoder.model_training(batch_xs)
-        loss_1 = autoencoder.get_loss(batch_xs)
-    if epoch % display_step == 0:
-        print("Epoch:",'%04d' %(epoch+1),"cost=","{:.9f}".format(loss),"cost2=","{:.9f}".format(loss_1))
-            
-print("Opt Done!")
-
-image_reconstruction = autoencoder.reconstruct(mnist.test.images[:examples_to_show])
-f, a = plt.subplots(2, 10, figsize=(10, 2))
-for i in range(examples_to_show):
-    a[0][i].imshow(np.reshape(mnist.test.images[i], (28, 28)))
-    a[1][i].imshow(np.reshape(image_reconstruction[i], (28, 28)))
-f.show()
-plt.draw()
-plt.waitforbuttonpress()
+#autoencoder = Autoencoder_2Layer(num_input = 784,
+#                          num_hidden_1 = 256,
+#                          num_hidden_2 = 128,
+#                          act_func = tf.nn.sigmoid,
+#                          optimizer = tf.train.RMSPropOptimizer(learning_rate = 0.01))
+#
+#training_epochs = 20
+#batch_size = 256
+#display_step = 1
+#examples_to_show = 10
+#total_batch = int(mnist.train.num_examples/batch_size)
+#
+#for epoch in range(training_epochs):
+#    for i in range(total_batch):
+#        batch_xs, batch_ys = mnist.train.next_batch(batch_size)
+#        loss = autoencoder.model_training(batch_xs)
+#        loss_1 = autoencoder.get_loss(batch_xs)
+#    if epoch % display_step == 0:
+#        print("Epoch:",'%04d' %(epoch+1),"cost=","{:.9f}".format(loss),"cost2=","{:.9f}".format(loss_1))
+#            
+#print("Opt Done!")
+#
+#
+#image_reconstruction = autoencoder.reconstruct(mnist.test.images[:examples_to_show])
+#f, a = plt.subplots(2, 10, figsize=(10, 2))
+#for i in range(examples_to_show):
+#    a[0][i].imshow(np.reshape(mnist.test.images[i], (28, 28)))
+#    a[1][i].imshow(np.reshape(image_reconstruction[i], (28, 28)))
+#f.show()
+#plt.draw()
+#plt.waitforbuttonpress()
